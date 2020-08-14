292f2db12f08b1a0512948245883cbbff3581214
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2020-08-14 14:36:21 +0300

    1.0.9

--- bot.py
+++ bot.py
@@ -27,7 +27,10 @@ async def info(ctx, name):
     achievementPoints = hypixel.get_achievementPoints(name, data)
     version = hypixel.get_mcVersionRp(name, data)
     firstLogin = hypixel.get_firstLogin(name, data)
-    lastLogin = hypixel.get_lastLogin(name, data)
+    try:
+        lastLogin = hypixel.get_lastLogin(name, data)
+    except:
+        lastLogin = "Unknown"
     dt_firstLogin = datetime.datetime.utcfromtimestamp(round(firstLogin/1000))
     dt_lastLogin = datetime.datetime.utcfromtimestamp(round(lastLogin/1000))
     # quests = hypixel.get_quests(name, data)

f881cfacd54b1a86b9d4bac779ca9da97205500b
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2020-08-19 20:33:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -207,10 +207,10 @@ async def chat(ctx, *, message):
     await ctx.send(f'{Message}')
 
 
- @bot.command()
- async def seth(ctx):
+@bot.command()
+async def seth(ctx):
     
-     await ctx.send(f'lmao the guy who stays you mean?')
+    await ctx.send(f'lmao the guy who stays you mean?')
 
 
 

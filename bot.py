5f30d4d5ebdf0a3da8cc20ee5bbdae64392321bc
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2020-09-26 15:47:13 +0300

    1.9.4

--- bot.py
+++ bot.py
@@ -228,10 +228,8 @@ async def dc(ctx):
         await ctx.send(f'Only Guild Members Have Access To This Command!')
  
 		    
-@bot.command(pass_context=True, aliases=['discord'])
-
+@bot.command()
 async def apply(ctx):
-
     await ctx.send(f'https://hypixel.net/threads/%E2%98%95%EF%B8%8F-jagares-jag-%E2%98%95%EF%B8%8F-level-89-%E2%98%95%EF%B8%8F-mega-walls-more-%E2%98%95%EF%B8%8F-top-100-%E2%98%95%EF%B8%8F-events-%E2%98%95%EF%B8%8F.2490194/')
 		    
 		    

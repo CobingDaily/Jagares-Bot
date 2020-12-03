25a1156ee01181b8ef482defd94db37affccd0b3
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2020-12-03 13:01:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,56 +46,57 @@ async def servers(ctx):
 
 @bot.command()
 async def info(ctx, name):
-    try:
-        data = hypixel.hypixel_api(name)
-        data2 = hypixel.hypixel_api2(name)
+    # try:
+    #     data = hypixel.hypixel_api(name)
+    #     data2 = hypixel.hypixel_api2(name)
 
-        ign = hypixel.get_displayname(name, data)
+    #     ign = hypixel.get_displayname(name, data)
 
-        level = hypixel.get_level(name, data2)
-        karma = hypixel.get_karma(name, data2)
-        achievementPoints = hypixel.get_achievementPoints(name, data2)
-        quests = hypixel.get_quests(name, data2)
-    except:
-        await ctx.send("Player not found! (Make sure to use their Minecraft username)")
-    version = hypixel.get_mcVersionRp(name, data)
-    firstLogin = hypixel.get_firstLogin(name, data)
+    #     level = hypixel.get_level(name, data2)
+    #     karma = hypixel.get_karma(name, data2)
+    #     achievementPoints = hypixel.get_achievementPoints(name, data2)
+    #     quests = hypixel.get_quests(name, data2)
+    # except:
+    #     await ctx.send("Player not found! (Make sure to use their Minecraft username)")
+    # version = hypixel.get_mcVersionRp(name, data)
+    # firstLogin = hypixel.get_firstLogin(name, data)
    
-    dt_firstLogin = datetime.datetime.utcfromtimestamp(round(firstLogin/1000))
-    try:
-        lastLogin = hypixel.get_lastLogin(name, data)
-        dt_lastLogin = datetime.datetime.utcfromtimestamp(round(lastLogin/1000))
-    except:
-        lastLogin = "Unknown"
-        dt_lastLogin = "Unknown"
+    # dt_firstLogin = datetime.datetime.utcfromtimestamp(round(firstLogin/1000))
+    # try:
+    #     lastLogin = hypixel.get_lastLogin(name, data)
+    #     dt_lastLogin = datetime.datetime.utcfromtimestamp(round(lastLogin/1000))
+    # except:
+    #     lastLogin = "Unknown"
+    #     dt_lastLogin = "Unknown"
 
 
 
-    embed = discord.Embed(
-    title = 'Jagares Bot',
-    colour = discord.Colour.orange()
-    )
+    # embed = discord.Embed(
+    # title = 'Jagares Bot',
+    # colour = discord.Colour.orange()
+    # )
 
 
 
-    # embed.set_author(name='w')
-    embed.add_field(name='Ign', value=f'{ign}', inline=False)
-    embed.add_field(name='Level', value=f'{level}', inline=False)
-    embed.add_field(name='Karma', value=f"{'{:,}'.format(karma)}", inline=False)
-    embed.add_field(name='Achievement Points', value=f"{'{:,}'.format(achievementPoints)}", inline=False)
-    embed.add_field(name='Quests Completed', value=f"{'{:,}'.format(quests)}", inline=False)
-    embed.add_field(name='Version', value=f'{version}', inline=False)
-    embed.add_field(name='First Login', value=f'{dt_firstLogin} UTC', inline=False)
-    embed.add_field(name='Last Login', value=f'{dt_lastLogin} UTC', inline=False)
-    embed.set_image(url=f"https://minotar.net/armor/body/{ign}.png")
-    embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
+    # # embed.set_author(name='w')
+    # embed.add_field(name='Ign', value=f'{ign}', inline=False)
+    # embed.add_field(name='Level', value=f'{level}', inline=False)
+    # embed.add_field(name='Karma', value=f"{'{:,}'.format(karma)}", inline=False)
+    # embed.add_field(name='Achievement Points', value=f"{'{:,}'.format(achievementPoints)}", inline=False)
+    # embed.add_field(name='Quests Completed', value=f"{'{:,}'.format(quests)}", inline=False)
+    # embed.add_field(name='Version', value=f'{version}', inline=False)
+    # embed.add_field(name='First Login', value=f'{dt_firstLogin} UTC', inline=False)
+    # embed.add_field(name='Last Login', value=f'{dt_lastLogin} UTC', inline=False)
+    # embed.set_image(url=f"https://minotar.net/armor/body/{ign}.png")
+    # embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
 
 
 
-    if level == 0:
-        await ctx.send("Player not found! (Make sure to use their Minecraft username)")
-    else:
-         await ctx.send(embed=embed)
+    # if level == 0:
+    #     await ctx.send("Player not found! (Make sure to use their Minecraft username)")
+    # else:
+    #      await ctx.send(embed=embed)
+    await ctx.send("Command Currently Unavailable")
 
 
 
@@ -630,44 +631,45 @@ async def mwclass(ctx, Class, name):
 
 @bot.command()
 async def guild(ctx, name):
-    gdata = hypixel.hypixel_gapi(name)
-    try:
-        guild_name = hypixel.get_guild_name(name, gdata)
-    except:
-        await ctx.send("Guild not found! (Make sure to use the guild name)")
-    guild_tag = hypixel.get_guild_tag(name, gdata)
-    guild_description = hypixel.get_guild_description(name, gdata)
-    guild_exp = hypixel.get_guild_exp(name, gdata)
-    guild_date = datetime.datetime.utcfromtimestamp(round(hypixel.get_guild_date(name, gdata)/1000))
-    guild_exp_history = hypixel.get_guild_exp_history(name, gdata)
+    # gdata = hypixel.hypixel_gapi(name)
+    # try:
+    #     guild_name = hypixel.get_guild_name(name, gdata)
+    # except:
+    #     await ctx.send("Guild not found! (Make sure to use the guild name)")
+    # guild_tag = hypixel.get_guild_tag(name, gdata)
+    # guild_description = hypixel.get_guild_description(name, gdata)
+    # guild_exp = hypixel.get_guild_exp(name, gdata)
+    # guild_date = datetime.datetime.utcfromtimestamp(round(hypixel.get_guild_date(name, gdata)/1000))
+    # guild_exp_history = hypixel.get_guild_exp_history(name, gdata)
 
-    embed = discord.Embed(
-    title = 'Jagares Bot',
-    colour = discord.Colour.orange()
-    )
+    # embed = discord.Embed(
+    # title = 'Jagares Bot',
+    # colour = discord.Colour.orange()
+    # )
 
 
 
-    # embed.set_author(name='w')
-    embed.add_field(name='Guild Name', value=f'{guild_name} \u200b', inline=False)
-    embed.add_field(name='Guild Tag', value=f'{guild_tag} \u200b', inline=False)
-    embed.add_field(name='Guild description', value=f'{guild_description} \u200b', inline=False)
-    embed.add_field(name='Guild Experience', value=f"{'{:,}'.format(guild_exp)} \u200b", inline=False)
-    embed.add_field(name='Creation Date', value=f'{guild_date} UTC', inline=False)
-    for key, value in guild_exp_history.items():
-        if value is None:
-            value = 0
-        embed.add_field(name=f'Experience Earned On {key} \u200b', value=f"{'{:,}'.format(value)} \u200b", inline=False)
+    # # embed.set_author(name='w')
+    # embed.add_field(name='Guild Name', value=f'{guild_name} \u200b', inline=False)
+    # embed.add_field(name='Guild Tag', value=f'{guild_tag} \u200b', inline=False)
+    # embed.add_field(name='Guild description', value=f'{guild_description} \u200b', inline=False)
+    # embed.add_field(name='Guild Experience', value=f"{'{:,}'.format(guild_exp)} \u200b", inline=False)
+    # embed.add_field(name='Creation Date', value=f'{guild_date} UTC', inline=False)
+    # for key, value in guild_exp_history.items():
+    #     if value is None:
+    #         value = 0
+    #     embed.add_field(name=f'Experience Earned On {key} \u200b', value=f"{'{:,}'.format(value)} \u200b", inline=False)
     
 
-    embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
+    # embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
 
 
 
-    if guild_name == 0:
-        await ctx.send("Guild not found!")
-    else:
-         await ctx.send(embed=embed)
+    # if guild_name == 0:
+    #     await ctx.send("Guild not found!")
+    # else:
+    #      await ctx.send(embed=embed)
+    await ctx.send("Command Currently Unavailable")
 
 
 

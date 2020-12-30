78e7e98762b56de118926f00d37dc138b00d5fc1
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2020-12-30 11:36:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -791,7 +791,7 @@ async def mwclass(ctx, Class, name=None):
 
     embed.add_field(name=f'{Class.capitalize()} Final Kills', value=f"`{'{:,}'.format(class_final_kills_all)}`", inline=True)
     embed.add_field(name=f'{Class.capitalize()} Final Assists', value=f"`{'{:,}'.format(class_final_assists_all)}`", inline=True)
-    embed.add_field(name=f'{Class.capitalize()} FK/D Ratio', value=f"`{'{:,}'.format(class_fkd)}`", inline=False)
+    embed.add_field(name=f'{Class.capitalize()} FK/D Ratio', value=f"`{'{:,}'.format(class_fkd)}`", inline=True)
     embed.add_field(name=f'{Class.capitalize()} W/L Ratio', value=f"`{'{:,}'.format(class_wlr)}`", inline=True)   
     embed.add_field(name=f'{Class.capitalize()} Finals For {class_next_fkd} FK/D Ratio', value=f"`{'{:,}'.format(class_need_fks)}`", inline=False)
     embed.add_field(name=f'{Class.capitalize()} Wins', value=f"`{'{:,}'.format(class_wins_all)}`", inline=True)

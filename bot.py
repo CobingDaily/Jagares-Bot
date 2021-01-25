072afedfe4c1eb2c6d83cde70c1c80af75bb0d7e
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-01-26 00:16:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -745,7 +745,7 @@ async def cp(ctx, name=None):
             if classpoints_for_class[k] == i:
                 sorted_dict[k] = classpoints_for_class[k]
                 break
-    colors = ["#524656", "#cf4647", "#eb7b59", "#a7c5bd"]
+    colors = ["#af87bd", "#cf4647", "#eb7b59", "#a7c5bd"]
     class_names_pie = list(sorted_dict.keys())
     class_values_pie = list(sorted_dict.values())       
     sizes = (class_values_pie[0], class_values_pie[1], class_values_pie[2], total_class_points-(class_values_pie[0] + class_values_pie[1] + class_values_pie[2]))

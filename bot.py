8a0673316d3cc9809e9d92d42599380b6fd32235
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-01-26 00:20:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -745,19 +745,19 @@ async def cp(ctx, name=None):
             if classpoints_for_class[k] == i:
                 sorted_dict[k] = classpoints_for_class[k]
                 break
-    colors = ["#af87bd", "#cf4647", "#eb7b59", "#a7c5bd"]
+    colors = ["#5d9db6", "#cf4647", "#eb7b59", "#a7c5bd"]
     class_names_pie = list(sorted_dict.keys())
     class_values_pie = list(sorted_dict.values())       
     sizes = (class_values_pie[0], class_values_pie[1], class_values_pie[2], total_class_points-(class_values_pie[0] + class_values_pie[1] + class_values_pie[2]))
     labels = (class_names_pie[0], class_names_pie[1], class_names_pie[2], "Other")
+    if len(class_names_pie) > 3:
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, shadow=False, startangle=90, autopct='%1.1f%%', colors=colors)
+        ax1.axis('equal')
 
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels=labels, shadow=False, startangle=90, autopct='%1.1f%%', colors=colors)
-    ax1.axis('equal')
-
-    plt.savefig("piechart.png", transparent=False, bbox_inches='tight')
-    img = discord.File("piechart.png", filename="piechart.png")
-    embed.set_image(url="attachment://piechart.png")
+        plt.savefig("piechart.png", transparent=False, bbox_inches='tight')
+        img = discord.File("piechart.png", filename="piechart.png")
+        embed.set_image(url="attachment://piechart.png")
 
     embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
 

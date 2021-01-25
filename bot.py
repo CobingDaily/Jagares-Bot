2b87dcf907c0dd0e430bbdc5a806fa900f032e65
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-01-25 15:09:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1164,10 +1164,10 @@ Level ➠ **{'{:,}'.format(guild_level)}**
 
 
 
-        embed.add_field(name=f'Stats', value=guild_stats, inline=True)
-        embed.add_field(name=f'Weekly GEXP', value=expValue, inline=False)
-        embed.add_field(name=f'Ranks', value=guild_ranks, inline=True)
-        embed.add_field(name=f"{player_ign}'s Weekly GEXP", value=playerExp, inline=False)
+        embed.add_field(name=f'Stats', value=guild_stats, inline=False)
+        embed.add_field(name=f'Weekly GEXP', value=expValue, inline=True)
+        embed.add_field(name=f'Ranks', value=guild_ranks, inline=False)
+        embed.add_field(name=f"{player_ign}'s Weekly GEXP", value=playerExp, inline=True)
         
 
         expEachDay.reverse()

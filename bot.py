cce3f1cfbcc89ef53027fb1ff858f879ba8fb12a
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-01-26 00:14:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -745,7 +745,7 @@ async def cp(ctx, name=None):
             if classpoints_for_class[k] == i:
                 sorted_dict[k] = classpoints_for_class[k]
                 break
-    colors = ["#f45d4c", "#f7a541", "#faca66", "#a1dbb2"]
+    colors = ["#524656", "#cf4647", "#eb7b59", "#a7c5bd"]
     class_names_pie = list(sorted_dict.keys())
     class_values_pie = list(sorted_dict.values())       
     sizes = (class_values_pie[0], class_values_pie[1], class_values_pie[2], total_class_points-(class_values_pie[0] + class_values_pie[1] + class_values_pie[2]))
@@ -761,7 +761,10 @@ async def cp(ctx, name=None):
 
     embed.set_footer(text="© 2020 LazBoi All Rights Reserved ")
 
-    await ctx.send(file=img, embed=embed)
+    if len(class_names_pie) > 3:
+        await ctx.send(file=img, embed=embed)
+    else:
+        await ctx.send(file=img, embed=embed)
 
 
 

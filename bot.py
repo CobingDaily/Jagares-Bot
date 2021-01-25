44975a20833df03f45cb4fdc4a3e618f28a1f1cf
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-01-25 14:52:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1117,7 +1117,7 @@ async def guild(ctx, name=None):
                 if member["uuid"] == player_uuid:
                     for dailyPlayerExpHistory in member["expHistory"]:
 
-                        dailyPlayerExp = dailyPlayerExpHistory[l]
+                        dailyPlayerExp = dailyPlayerExpHistory
                         print(dailyExpHistory)
                         playerExp += f"{dailyPlayerExpHistory} ➠ **{'{:,}'.format(dailyPlayerExp)}** \n"
                         l += 1

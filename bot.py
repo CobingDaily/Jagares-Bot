5b660cd0df19fa9ba72046c2098418f068581b1a
wtflol123 <69477634+wtflol123@users.noreply.github.com>
2021-02-26 23:12:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1529,10 +1529,7 @@ async def bsg(ctx, name=None, kit=None):
         name = ctx.message.author.display_name
 
 
-    if kit.lower() == "horse":
-        kit = "horsetamer"
-        if name is None:
-            name = ctx.message.author.display_name
+
 
     data = hypixel.hypixel_api(name)
     try:
